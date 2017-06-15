213cd2c9074b60f4d0b3cf0f81f9df808e3b4034
Jan Schwinghammer <j.schwinghammer@gmail.com>
2017-06-15 22:12:38 +0200

    initial commit

---
+++ UnitTests/UnitTestAttribute.cs
@@ -0,0 +1,23 @@
+﻿namespace UnitTests
+{
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Attribute to mark a <see cref="Microsoft.VisualStudio.TestTools.UnitTesting"/> as unit test
+    /// </summary>
+    /// <seealso cref="Microsoft.VisualStudio.TestTools.UnitTesting.TestCategoryBaseAttribute" />
+    public class UnitTestAttribute : TestCategoryBaseAttribute
+    {
+        /// <summary>
+        /// Gets the test category name.
+        /// </summary>
+        public override IList<string> TestCategories
+        {
+            get
+            {
+                return new List<string>() { "UnitTest" };
+            }
+        }
+    }
+}
